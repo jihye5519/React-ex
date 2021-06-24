6071ddda47ea99a1329b02b6a3c04025aa94bad5
jihye5519 <2721875@naver.com>
2021-06-24 16:59:00 +0900

    컴포넌트에 ref달기 예제

---
+++ src/ScrollBox.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class ScrollBox extends Component {
+
+    render(){
+        const style = {
+            border: '1px solid black',
+            height: '300px',
+            width  : '300px',
+            overflow : 'auto',
+            position : 'relative'
+        };
+
+        const innerStyle = {
+            width : '100%',
+            height : '650%',
+            background : 
+        }
+    }
+}
+
+export default ScrollBox;
